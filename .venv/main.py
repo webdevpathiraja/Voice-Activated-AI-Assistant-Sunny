3b585ccde8e40fd6dc61ca36ae3bcbbda47f4681
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-02-03 21:42:47 +0530

    improved error handling

--- .venv/main.py
+++ .venv/main.py
@@ -1,15 +1,45 @@
 import speech_recognition as sr
 import pyttsx3
+import wikipedia
+import pywhatkit
 
-Listener = sr.Recognizer()
-engine = pyttsx3.init()
+listener = sr.Recognizer()
+player = pyttsx3.init()
 
-with sr.Microphone() as input_device:
-    print("Listening...Say something!")
-    voice_content = Listener.listen(input_device)
-    text = Listener.recognize_google(voice_content)
-    print(text)
 
-engine.say("Hello, I'm VoiceBot.")
-engine.runAndWait()
+def listen():
+    with sr.Microphone() as input_device:
+        print("I am ready, Listening ....")
+        voice_content = listener.listen(input_device)
+        text_command = listener.recognize_google(voice_content)
+        text_command = text_command.lower()
+        print(text_command)
 
+    return text_command;
+
+
+def talk(text):
+    player.say(text)
+    player.runAndWait()
+
+
+def run_voice_bot():
+     command = listen()
+     if "sunny" in command:
+        command = command.replace("sunny","")
+        if "what is" in command:
+            command = command.replace("what is", "")
+            info = wikipedia.summary(command, sentences = 5)
+            talk(info)
+        elif "who is" in command:
+            command = command.replace("who is", "")
+            info = wikipedia.summary(command, sentences = 5)
+            talk(info)
+        elif "play" in command:
+            command = command.replace("play", "")
+            pywhatkit.playonyt(command)
+        else:
+            talk("Sorry, I am unable to find what you looking for")
+
+
+run_voice_bot()
